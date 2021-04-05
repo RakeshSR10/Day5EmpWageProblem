7df1aae16a84058ed6cfed2e9d79c4be8c65c4b7
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-05 10:34:19 +0530

    [Modified] added Daily Wage to EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,26 @@
 Welcome to Employee Wage Computation Program
-public class EmployeeWage {
-
-	public static void main(String[] args) 
+public class EmployeeWage 
+{
+public static void main(String[] args) 
 	{
+		//Constants
 		int IS_FULL_TIME = 1;
-		//Computation
+		int EMP_PER_RATE_HOUR = 20;
+
+		//Variables
+		int empHrs=0;
+		int empWage=0;
+
+		//Computation and full time
 		double empCheck = Math.floor(Math.random() *10)%2;
 		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+			empHrs=8;
 		else
-			System.out.println("Employee is Absent");
+			empHrs=0;
+
+		empWage = empHrs * EMP_PER_RATE_HOUR;
+		System.out.println("Employee wage = "+empWage);
 	}
+
 
 }
